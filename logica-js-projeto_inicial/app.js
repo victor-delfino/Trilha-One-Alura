7ff96551fa5c1cd5ef029d9ca56da4dcd6ad0ae7
victor-delfino <hugo.delfino11@gmail.com>
2024-01-25 15:49:43 -0300

    Lógica e sintaxe simples Js

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,28 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute;
+let tentativas = 1;
+
+while (chute !== numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 a ${numeroMaximo}`);
+
+    if (chute  == numeroSecreto) {
+        break;
+        
+    } else if (chute > numeroSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
+    tentativas++;
+} 
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Descobriu o número ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+/* if(tentativas > 1)
+alert(`Isso aí! Descobriu o número ${numeroSecreto} com ${tentativas} tentativas`);
+else {
+    alert(`Isso aí! Descobriu o número ${numeroSecreto} com ${tentativas} tentativa`);
+} */
